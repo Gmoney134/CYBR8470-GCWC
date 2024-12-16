afa889676c3455a6a67463b8c4784edef736dcb4
Grant Nichter <nichter.grant@gmail.com>
2024-12-15 21:07:25 -0600

    move login page from typescript to js for deployment purposes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,14 +2,14 @@ import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import styles from './page.module.css';
-import logo from './GCWC_logo.png'
+import logo from './GCWC_logo.png';
 import Image from 'next/image';
 
 const API_BASE_URL = 'http://backend:8000';
 
-export async function handleLogin(formData: any[]) {
+export async function handleLogin(formData) {
   'use server';
-  let redirectPath: string | null = null
+  let redirectPath = null;
 
   const { username, password } = Object.fromEntries(formData.entries());
   const loginUrl = `${API_BASE_URL}/GCWC/login/`;
@@ -35,22 +35,20 @@ export async function handleLogin(formData: any[]) {
     const data = await response.json();
 
     // Set the cookie securely
-    (await
-      cookies()).set('token', data.access, {
+    (await cookies()).set('token', data.access, {
       path: '/',
       httpOnly: true,
     });
 
-    redirectPath = `/profile`
+    redirectPath = `/profile`;
   } catch (error) {
     console.error('Login failed:', error); // Log the full error object
     throw new Error(`Error during login: ${error.message || 'Unknown error'}. Check console for details.`);
-    redirectPath = `/`
+    redirectPath = `/`;
   } finally {
-    //Clear resources
-    if (redirectPath)
-        redirect(redirectPath)
-}
+    // Clear resources
+    if (redirectPath) redirect(redirectPath);
+  }
 }
 
 export default function LoginPage() {
@@ -65,15 +63,16 @@ export default function LoginPage() {
 
       {/* Login Form */}
       <div className={styles.formContainer}>
-      <form action={handleLogin}>
-        <input type="text" name="username" placeholder="Username" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Don't have an account? <Link href="/register">Register here</Link>
-      </p>
+        <form action={handleLogin}>
+          <input type="text" name="username" placeholder="Username" required />
+          <input type="password" name="password" placeholder="Password" required />
+          <button type="submit">Login</button>
+        </form>
+        <p>
+          Don't have an account? <Link href="/register">Register here</Link>
+        </p>
       </div>
     </div>
   );
 }
+
